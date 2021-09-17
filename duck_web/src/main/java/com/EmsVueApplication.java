0bc0d837eed2e3b40471020c68e49c82caaa6d3c
Meng Chen <chenmeng_1919@163.com>
2021-09-17 23:32:39 +0800

    项目初始化

---
+++ duck_web/src/main/java/com/EmsVueApplication.java
@@ -0,0 +1,13 @@
+/*package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuckNestApplication.class, args);
+    }
+
+}*/
